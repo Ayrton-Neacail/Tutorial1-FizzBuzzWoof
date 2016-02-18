17219d945435e0bb8ad89b32909d3737c8aeb337
Ayrton-Neacail <ayrtona90@gmail.com>
2016-02-18 13:36:38 +0200

    coding for loop and if statement

---
+++ Tutorial1/Tutorial1/fizzbuzzwoof.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int count = 0;
+
+	for (count = 0; count < 21; count++)
+
+		if (count % 3 = 0){
+
+			cout << count = "fizz"
+		}
+		else (count % 5 = 0){
+			cout << count = "Buzz"
+		}
+
+}
